0ac4728f84fd6a4b4332f4bd2a9130b6cb45be7c
Benjamin Miles <dootbin@benjamins-macbook-pro.local>
2019-03-02 08:04:15 -0600

    Initial Commit

---
+++ HipList_bmiles/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HipList_bmiles
+//
+//  Created by Benjamin Miles on 3/2/19.
+//  Copyright © 2019 Benjamin Miles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
